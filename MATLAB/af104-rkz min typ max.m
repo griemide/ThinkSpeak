d874e7c0bf7a9b14301d27a5356073c224da675c
Michael Gries <github@gries.name>
2017-07-11 22:37:49 +0200

    17.7.11
    C:\Users\Public\Documents\Projects\ThingSpeak\MATLAB\Visualization

---
+++ MATLAB/af104-rkz min typ max.m
@@ -0,0 +1,63 @@
+% af104-rkz
+% M. Gries, 2017-07-11
+% used for channel application af104-rkz min-typ-max values
+% code derived of af104-fsk
+% Field 2 =  'D1 Pulse'
+% http://www.mathworks.com/help/matlab/ref/annotationtextbox-properties.html
+
+channel = 296355;
+fields = 2;
+NumPoints = 1000;
+fieldSamples = thingSpeakRead(channel, 'Fields', fields, 'NumPoints', NumPoints);
+
+% Channel color used in channel field config window: #317db5
+% r = hex2dec('31')/255;
+% g = hex2dec('7d')/255;
+% b = hex2dec('b5')/255;
+
+% use decimal values if pipette fuction used in windows paint app
+r = 165/255;
+g = 40/255;
+b = 41/255;
+textColor = [r g b]  % print output
+
+% fieldSamples array may contain Not A Number (NaN) values
+% function mean will result in a NaN result if array contain one or more NaN
+% due to licencing reasons for function nanmean use following workaround
+fieldSamples(isnan(fieldSamples)) = [];
+% hampel(fieldSamples, 10);   % only registered users
+B = smoothdata(fieldSamples);
+
+maxValue = max(fieldSamples);
+meanValue = mean(fieldSamples);
+minValue = min(fieldSamples);
+
+UOM = ' msec';
+UOMmin = [' ' UOM ' (min)'];
+UOMmax = [' ' UOM ' (max)'];
+
+annotation('textbox',[0.0 0.4 0.9 0.6],...
+ 'HorizontalAlignment','right',...
+ 'VerticalAlignment','middle',...
+ 'LineStyle','none',...
+ 'String',[num2str(maxValue,3) UOMmax],...
+ 'Color','0.5 0.5 0.5',...
+ 'FontSize',28);
+ 
+annotation('textbox',[0.0 0.2 0.9 0.6],...
+ 'HorizontalAlignment','right',...
+ 'VerticalAlignment','middle',...
+ 'LineStyle','none',...
+ 'String',[num2str(meanValue,3) UOM],...
+ 'Color',textColor,...
+ 'FontSize',72);
+ 
+annotation('textbox',[0.0 0.0 0.9 0.6],...
+ 'FontName','FixedWidth',...
+ 'HorizontalAlignment','right',...
+ 'VerticalAlignment','middle',...
+ 'LineStyle','none',...
+ 'String',[num2str(minValue,3) UOMmin],...
+ 'Color','0.5 0.5 0.5',...
+ 'FontSize',28);
+ 
