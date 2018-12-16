17e683314cd8e20f639ae1383c89b547975b6fa7
Michael Gries <github@gries.name>
2018-12-16 20:50:21 +0100

    18.12.16

--- MATLAB/af104-fsz histogram.m
+++ MATLAB/af104-fsz histogram.m
@@ -1,15 +1,26 @@
 % af104-fsz histogram (Ferraris-Strom-Zähler)
 
+% references (histogram ): 
+% https://de.mathworks.com/help/matlab/ref/matlab.graphics.chart.primitive.histogram.html
+
 author   = 'M. Gries'; author
 created  = '18.12.6'; 
-modified = '18.12.6';
-version  = '18.12.6'; version
+modified = '18.12.16';
+version  = '18.12.16'; version
 
 readChannelID = 624220; % af104-fsz
 FieldID = 2;
 histogramTitle = 'Histogram Ferraris Type C114';
 histogramXlabel = 'pulse width [msec]';
 histogramYlabel = 'Number of pulses';
+% define limits
+histogramBinLowerLimit = 0;
+histogramBinUpperLimit = 14000; % evaluated maximn ist approx 12850 msec
+histogramBinSteps = 250;
+histogamBinLimits = [histogramBinLowerLimit, histogramBinUpperLimit];
+histogramNoOfBins = (histogramBinUpperLimit-histogramBinLowerLimit)/histogramBinSteps; 
+% alternative (use BinWidth property)
+histogramBinWidth = 250; 
 
 [pulses, timeStamp] = thingSpeakRead(readChannelID, 'Fields', FieldID, 'NumMinutes', 24*60); 
 
@@ -33,10 +44,13 @@ display(cleanData, 'Cleaned data');
 display(anyOutliers, 'No of outliners');
 display(numel(cleanData), 'No pulses after cleaning');
 
-histogram(cleanData);
+% h = histogram(cleanData, histogramNoOfBins, 'BinLimits',[0 14000]);
+h = histogram(cleanData, 'BinWidth', histogramBinWidth);
 title(histogramTitle);
 xlabel(histogramXlabel);
 ylabel(histogramYlabel);
 grid on
+
+display (h, 'histogram properties');
 
 % EOF
