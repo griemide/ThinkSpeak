9de0b71796bd0ceb79348de64f554c755bb9bc8d
Michael Gries <github@gries.name>
2018-12-23 16:39:23 +0100

    18.12.23

--- MATLAB/Download ThingSpeak data to CSV.m
+++ MATLAB/Download ThingSpeak data to CSV.m
@@ -2,8 +2,8 @@
 
 author   = 'M. Gries'; author
 created  = '18.12.22'; % perform multiple downloads due to 8000 points limitations
-modified = '18.12.22'; % tested with channel af104-fsz
-version  = '18.12.22'; version
+modified = '18.12.23'; % tested with channel af104-fsz
+version  = '18.12.23'; version
 
 % Implemented download strategy:
 % { preconditions are: 
@@ -20,24 +20,26 @@ version  = '18.12.22'; version
 %  7. determine number of records read 
 
 
-% https://www.mathworks.com/examples/matlab/mw/matlab-ex71355721-export-table-to-text-file
 
 % references (af104-fsz histogram over time): 
 % https://thingspeak.com/apps/matlab_visualizations/264686/edit
 
+% https://de.mathworks.com/help/thingspeak/thingspeakread.html
 readChannelID = 624220; % af104-fsz
 FieldID = 3; % KWh counter
 
 % datasets (NumPoints) are limited to 8000 for a free licence 
-% get Data with corresponding Timestamp and Channel information first
+MaxRecordsPerRead = 8000;
+% therefore get Data with corresponding Timestamp and Channel information first
 [data,Timestamps,chInfo] = thingSpeakRead(readChannelID); % recent variables
 display(chInfo, 'ThinkSpeak channel information');
 display(chInfo.Created, 'ThinkSpeak channel earliest possible date');
 display(chInfo.LastEntryID, 'ThinkSpeak channel records in total');
 
-% https://de.mathworks.com/help/thingspeak/thingspeakread.html
-% 'DateRange',[datetime('Aug 8, 2014'),datetime('Aug 12, 2014')]
+TotalNoOfRecords = chInfo.LastEntryID;
+Periods  = TotalNoOfRecords / MaxRecordsPerRead;
 
+% ...thinkSpeakRead... 'DateRange',[datetime('Aug 8, 2014'),datetime('Aug 12, 2014')]
 % When you perform operations involving datetime arrays, the arrays either must all have a time zone associated with them, 
 % or they must all have no time zone.
 DateCreated = chInfo.Created; DateCreated
@@ -50,14 +52,14 @@ DateRangePeriod = [DateStart, DateEnd]; DateRangePeriod
 
 % [data,Timestamps,chInfo] = thingSpeakRead(readChannelID,'Fields',FieldID,'NumDays',64); 
 % [data,Timestamps] = thingSpeakRead(readChannelID,'Fields',FieldID,'NumPoints',8000); 
-% [TT,chinfo] = thingSpeakRead(readChannelID,'DateRange',DateRangePeriod,'Fields',FieldID,'OutputFormat','timetable'); 
-[T,chinfo] = thingSpeakRead(readChannelID,'Fields',FieldID,'NumDays',64,'OutputFormat','table'); 
+[TT,chinfo] = thingSpeakRead(readChannelID,'DateRange',DateRangePeriod,'Fields',FieldID,'OutputFormat','timetable'); 
+% [TT,chinfo] = thingSpeakRead(readChannelID,'Fields',FieldID,'NumDays',64,'OutputFormat','timetable'); 
 
-whos T
-T = rmmissing(T);
-whos T
-head(T)
-T1 = T.Timestamps(1)
+whos TT
+TT = rmmissing(TT);
+whos TT
+head(TT)
+T1 = TT.Timestamps(1)
 whos T1
 
 % https://de.mathworks.com/help/thingspeak/thingspeakread.html
@@ -67,17 +69,19 @@ DateEnd = datetime(T1);
 DateStart = datetime(T1 - days(5)); DateStart
 DateRangePeriod = [DateStart, DateEnd]; DateRangePeriod
 
-[U,chinfo] = thingSpeakRead(readChannelID,'DateRange',DateRangePeriod,'Fields',FieldID,'OutputFormat','table'); 
+[TU,chinfo] = thingSpeakRead(readChannelID,'DateRange',DateRangePeriod,'Fields',FieldID,'OutputFormat','timetable'); 
 
-U = rmmissing(U);
-whos U
-tail(U)
-head(U)
+TU = rmmissing(TU);
+whos TU
+tail(TU)
+head(TU)
 
-Tsummary = [U;T];
-whos T U 
+Tsummary = [TU;TT];
+whos TT TU 
 whos Tsummary
 Tsummary
+
+% https://www.mathworks.com/examples/matlab/mw/matlab-ex71355721-export-table-to-text-file
 
 %RT = readtable('C:\HC-PP\test.txt');
 %whos RT
@@ -103,5 +107,7 @@ end
 %whos H
 %H
 
+TotalNoOfRecords 
+Periods
 
 %EOF
