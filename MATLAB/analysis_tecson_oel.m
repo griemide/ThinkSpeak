a7e29b8eb4d22c840c3c0178ce5aa8e25231c96d
Michael Gries <github@gries.name>
2019-01-22 22:56:20 +0100

    19.1.22

---
+++ MATLAB/analysis_tecson_oel.m
@@ -0,0 +1,35 @@
+% analysis_tecson_oel
+
+author   = 'M. Gries'; author
+created  = '19.1.22 '; % granted from MATLAB Analysis example
+modified = '19.1.22 '; % used with oel supplier reference webpage
+version  = '19.1.22 '; version
+
+% based on following example
+% https://de.mathworks.com/help/thingspeak/read-live-web-data-for-vessels-at-the-port-of-boston.html
+
+% Specify the url containing information on current oel price [ct per liter]
+url = 'https://www.tecson.de/pheizoel.html';
+
+% TODO - Replace the [] with channel ID to write data to:
+writeChannelID = [];
+
+% TODO - Enter the Write API Key between the '' below:
+writeAPIKey = '';
+
+% Fetch data and parse 
+webText = webread(url);
+rawValue = urlfilter(webText, 'Referenzpreis heute:', 1); 
+display(rawValue, 'urlfilter result:');
+% value on webpage displayed with 1 decimal
+% but urlfilter extract without deccimal point
+% therefore devide by 1 decimal (i.e. 10)
+CentPerLiter = rawValue / 10;
+display(CentPerLiter, 'TECSON Referenzpreis heute:');
+%or calculate in Euro per 1000 Liter
+EuroPerCBM = CentPerLiter * 1000 / 100;
+display(EuroPerCBM, 'Euro per 1000 Liter:');
+
+% thingSpeakWrite(writeChannelID, CentPerLiter, 'Writekey', writeAPIKey);
+
+% EOF
