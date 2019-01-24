9213680ebbdf126e09ec060085d4d3436e90cef3
Michael Gries <github@gries.name>
2019-01-24 12:13:23 +0100

    19.1.24

--- MATLAB/analysis_tecson_oel.m
+++ MATLAB/analysis_tecson_oel.m
@@ -3,7 +3,8 @@
 author   = 'M. Gries'; author
 created  = '19.1.22 '; % granted from MATLAB Analysis example
 modified = '19.1.22 '; % used with oel supplier reference webpage
-version  = '19.1.22 '; version
+modified = '19.1.24 '; % combine new value with other channel fields
+version  = '19.1.24 '; version
 
 % based on following example
 % https://de.mathworks.com/help/thingspeak/read-live-web-data-for-vessels-at-the-port-of-boston.html
@@ -13,9 +14,15 @@ url = 'https://www.tecson.de/pheizoel.html';
 
 % TODO - Replace the [] with channel ID to write data to:
 writeChannelID = 310930; % af104-fsk eval
+readChannelID = writeChannelID;
 
 % TODO - Enter the Write API Key between the '' below:
 writeAPIKey = '4FX35MCBK93LDMKZ';
+
+% read last records for already (unchanged) values
+fields = 1;
+NumPoints = 1;
+lastRecord = thingSpeakRead(readChannelID, 'Fields', fields, 'NumPoints', NumPoints);
 
 % Fetch data and parse:
 %   <tr>
@@ -34,9 +41,8 @@ display(CentPerLiter, 'TECSON Referenzpreis heute:');
 EuroPerCBM = CentPerLiter * 1000 / 100;
 display(EuroPerCBM, 'Euro per 1000 Liter:');
 
-% combine two values fron writing into channel
-eval = [88.8 CentPerLiter]
-
-thingSpeakWrite(writeChannelID, eval, 'Writekey', writeAPIKey);
+% combine lastRecords value(s) before writing into channel
+eval = [lastRecord CentPerLiter]
+response=thingSpeakWrite(writeChannelID, eval, 'Writekey', writeAPIKey)
 
 % EOF
